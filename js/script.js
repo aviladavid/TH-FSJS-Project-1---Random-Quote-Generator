90590d84812c8204b4628a9404fc45df7ed238f8
David Avila <aviladavid@gmail.com>
2017-10-07 08:29:27 +0200

    Fixes 08
    Rearranged code blocks

--- js/script.js
+++ js/script.js
@@ -1,90 +1,8 @@
 /* Program that randomly selects a quote from an array and displays it to a web page.  */
 
-// Array containing the object quotes to be displayed on the web page.
-var quotes = [
-    {
-        quote: '"Live as if you were to die tomorrow. Learn as if you were to live forever."', // Text of the quote to be displayed on the page
-        source: '-Mahatma Gandhi-', // Identifies the creator of the quote. Who said it?. 
-        citation: '', // If known. Where the quote comes from (public speech, TV show, book, etc.)
-        year: '', // If known.  
-        tag: '-Life.' // humor, business, politics, love, inspirational, etc
-    },
-    {
-        quote: '"Don\'t cry because it\'s over. Smile because it happened."',
-        source: '- Dr. Seuss',
-        citation: ' ',
-        year: ' ',
-        tag: '-Life.'
-    },
-    {
-        quote: '“If you only read the books that everyone else is reading, you can only think what everyone else is thinking.”',
-        source: '-Haruki Murakami-',
-        citation: ' Norwegian Wood (Novel),',
-        year: ' (1987)',
-        tag: '-Wisdom.'
-    },
-    {
-        quote: '“The flower that blooms in adversity is the rarest and most beautiful of all.”',
-        source: '― Walt Disney Company-',
-        citation: ' Mulan (Movie).',
-        year: ' (1998)',
-        tag: '-Inspirational'
-    },
-    {
-        quote: '“Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.”',
-        source: '―Albert Einstein-',
-        citation: '',
-        year: '',
-        tag: '-Science.'
-    },
-    {
-        quote: '“Success consists of going from failure to failure without loss of enthusiasm.”',
-        source: '―Winston Churchill-',
-        citation: '',
-        year: '',
-        tag: '-Motivational.'
-    },
-    {
-        quote: '"It\'s the possibility of having a dream come true that makes life interesting."',
-        source: "-Paulo Coelho-",
-        citation: " The Alchemist.",
-        year: " (1988)",
-        tag: "-Inspirational."
-},
-    {
-        quote: '"You can fool all the people some of the time, and some of the people all the time, but you cannot fool all the people all the time."',
-        source: '-Abraham Lincoln-',
-        citation: '',
-        year: '',
-        tag: '-Life.'
-    },
-    {
-        quote: '"You are ghost driving a meat-coated skeleton made from stardust, riding a rock, hurtling through space. Fear Nothing!"',
-        source: '-Unknown-',
-        citation: '',
-        year: '',
-        tag: '-Epic internet stuff.'
-    }
-];
 
-var tempArray = []; // will temporarily hold the objects that will be used to pick a random quote
+var tempArray; // temporary copy of the quotes array
 var quoteTimer; // Variable for the 30 sec timer on printQuote
-
-// event listener to respond to "Show another quote" button clicks when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-// event listener to change background color on button click for the selected DOM elements
-var domElement1 = document.querySelector('body');
-var domElement2 = document.querySelector('#loadQuote');
-document.getElementById('loadQuote').addEventListener("click", () => {
-    var newColor1 = colorChange();
-    var newColor2 = colorChange();
-    console.log("BODY BACKGROUND COLOR: " + newColor1);
-    console.log("BUTTON BACKGROUND COLOR: " + newColor2);
-    domElement1.style.backgroundColor = newColor1;
-    domElement2.style.backgroundColor = newColor2;
-    console.log("------END------");
-});
 
 // getRandomQuote selects random quote object from the quotes array and returns the randomly selected object. 
 function getRandomQuote() {
@@ -148,7 +66,21 @@ function colorChange() {
 return randColor;
 };
 
+// event listener to respond to "Show another quote" button clicks when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// event listener to change background color on button click for the selected DOM elements
+var domElement1 = document.querySelector('body');
+var domElement2 = document.querySelector('#loadQuote');
+document.getElementById('loadQuote').addEventListener("click", () => {
+    var newColor1 = colorChange();
+    var newColor2 = colorChange();
+    console.log("BODY BACKGROUND COLOR: " + newColor1);
+    console.log("BUTTON BACKGROUND COLOR: " + newColor2);
+    domElement1.style.backgroundColor = newColor1;
+    domElement2.style.backgroundColor = newColor2;
+    console.log("------END------");
+});
 
 
 
